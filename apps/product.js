d9f9340e14d9a88da9da5223f079553dfe38dc62
Harvel Kay <codeit.harvel@gmail.com>
2023-10-09 18:40:55 +0300

    Added generating thumbnails on product update

--- apps/product.js
+++ apps/product.js
@@ -6,48 +6,55 @@ const productRoute = require("../utils/gen/miniApp")();
 const fs = require("node:fs");
 const config = require("config");
 const login = require("../middleware/auth");
+const { generateThumb } = require("../utils/gen/thumbnail");
 
 productRoute.get("/:current_p", login, async (req, res) => {
-
   // validate user login
-  const current_p = parseInt(req.params.current_p)
+  const current_p = parseInt(req.params.current_p);
   const pageSize = 3;
-  const startIndex = ( current_p -1 )*pageSize 
+  const startIndex = (current_p - 1) * pageSize;
 
-  const products = await Product.find().select(["-transactions", "-__v"]).skip(startIndex).limit(pageSize);
-  const total = await Product.countDocuments()
-  if (!products || !total ) return res.status(500).send("oops try again,Appologies..... ")
-  
-  const last_page = Math.ceil(total / pageSize)
-  
-  let page = 1
-  if( last_page !== 1)
-   page = current_p + 1
+  const products = await Product.find()
+    .select(["-transactions", "-__v"])
+    .skip(startIndex)
+    .limit(pageSize);
+  const total = await Product.countDocuments();
+  if (!products || !total)
+    return res.status(500).send("oops try again,Appologies..... ");
+
+  const last_page = Math.ceil(total / pageSize);
+
+  let page = 1;
+  if (last_page !== 1) page = current_p + 1;
 
   const response = {
-    products,page,last_page
-  }
+    products,
+    page,
+    last_page,
+  };
   res.send(response);
 });
 
+productRoute.get("/search/:name", async (req, res) => {
+  const query = new RegExp(req.params.name, "i");
 
-productRoute.get('/search/:name', async(req, res) => {
-  const query = new RegExp(req.params.name,'i')
-  
   const found = await Product.find({
-    name: {$regex:query }})
-  
-  if (!found) return res.status(500).send("Oops there might be something wrong !, try again")
+    name: { $regex: query },
+  });
 
-  res.send(found)
-})
+  if (!found)
+    return res
+      .status(500)
+      .send("Oops there might be something wrong !, try again");
+
+  res.send(found);
+});
 
 productRoute.post("/", login, async (req, res) => {
-
   const { error } = validateProduct(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const { name, genre_id, price, tag,thumbnail, numberInStock } = req.body;
+  const { name, genre_id, price, tag, thumbnail, numberInStock } = req.body;
 
   const nameTaken = await Product.findOne({ name: case_it(name) });
   if (nameTaken) return res.status(400).send("Duplicate name...");
@@ -64,7 +71,7 @@ productRoute.post("/", login, async (req, res) => {
     price,
     numberInStock,
     tag,
-    thumbnail
+    thumbnail,
   });
 
   await newProduct.save();
@@ -74,14 +81,23 @@ productRoute.post("/", login, async (req, res) => {
 productRoute.put("/:prod_id", login, async (req, res) => {
   // validate user permisions
 
-  const { numberInStock,name,price } = req.body;
+  const { numberInStock, name, price } = req.body;
+  const public_url = config.get("public_url");
 
   const exists = await Product.findById(req.params.prod_id);
   if (!exists) return res.status(404).send("Product doesn't exist");
 
+  let tnail_p = "";
+  if (!exists.thumbnail) {
+    // Generate Thumbnail
+    const file_path = exists.tag.replace(public_url, "public");
+    const thumb_p = file_path.replace("tags", "thumbnails");
+    tnail_p = generateThumb(file_path, thumb_p);
+  }
+
   const updated = await Product.findOneAndUpdate(
     { _id: exists._id },
-    { $set: { numberInStock,name,price, } },
+    { $set: { numberInStock, name, price, thumbnail: tnail_p } },
     { new: true }
   );
 
--- test.js
+++ test.js
@@ -1,13 +1,14 @@
-const sharp = require("sharp")
-const path = require("path")
-const fs = require("fs")
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
+const tracker = require("./log/logger");
 
-fs.appendFile("public/thumbnails/test.jpg", "" ,"utf8",()=>{})
+// fs.appendFile("public/thumbnails/test.jpg", "" ,"utf8",()=>{})
 
-const tnail = sharp('./public/test.jpg').resize(320)
-    .toFile('./public/thumbnails/test.jpg')
-    .then(data => console.log("Resize operation successful"))
-    .catch(err => console.log("Error resizing Image",err))
+// const tnail = sharp('./public/test.jpg').resize(320)
+//     .toFile('./public/thumbnails/test.jpg')
+//     .then(data => console.log("Resize operation successful"))
+//     .catch(err => console.log("Error resizing Image",err))
 
 // console.log("Resized thumbnail")
 // console.log("Resized executed")
